f063d9c08927e50272988d12a8e0d6e4d69f8bca
gjevardat <greg.jdf@gmail.com>
2024-10-10 08:54:32 +0200

    use env files instead of in code variables

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,11 +2,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user:'jevardat_local',
-  password: '!jevardat_local!',
-  host: 'localhost',
-  port: 55434,
-  database: 'surveys',
+  user:process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
 
 });
 
